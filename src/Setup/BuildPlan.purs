4b21930db7430c25308152cdf6f814068c5516d6
Thomas Honeyman <admin@thomashoneyman.com>
2020-10-03 17:03:55 -0700

    fix?

--- src/Setup/BuildPlan.purs
+++ src/Setup/BuildPlan.purs
@@ -5,11 +5,13 @@ import Prelude
 import Control.Monad.Except.Trans (ExceptT)
 import Data.Argonaut.Core (Json)
 import Data.Argonaut.Decode (decodeJson, printJsonDecodeError, (.:))
+import Data.Array as Array
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
 import Data.Foldable (fold)
+import Data.Maybe (Maybe(..))
 import Data.Newtype (unwrap)
-import Data.Traversable (for, traverse)
+import Data.Traversable (traverse)
 import Data.Version (Version)
 import Data.Version as Version
 import Effect (Effect)
@@ -29,47 +31,45 @@ type BuildPlan = Array { tool :: Tool, version :: Version }
 
 -- | Construct the list of tools that sholud be downloaded and cached by the action
 constructBuildPlan :: Json -> ExceptT Error Effect BuildPlan
-constructBuildPlan json = for Tool.allTools \tool -> do
-  liftEffect $ Core.info $ fold [ "Resolving ", Tool.name tool ]
-  res<- resolve json tool
-  liftEffect $ Core.info $ fold [ "Resolved ", Tool.name tool ]
-  pure res
+constructBuildPlan json = map Array.catMaybes $ traverse (resolve json) Tool.allTools
 
 -- | The parsed value of an input field that specifies a version
 data VersionField = Latest | Exact Version
 
 -- | Attempt to read the value of an input specifying a tool version
-getVersionField :: Key -> ExceptT Error Effect VersionField
+getVersionField :: Key -> ExceptT Error Effect (Maybe VersionField)
 getVersionField key = do
   value <- Core.getInput' (unwrap key)
   case value of
-    "latest" -> pure Latest
+    "" ->
+      pure Nothing
+    "latest" ->
+      pure (pure Latest)
     val -> case Version.parseVersion val of
-      Left msg -> throwError (error (ParseError.parseErrorMessage msg))
-      Right version -> pure (Exact version)
+      Left msg ->  do
+        liftEffect $ Core.error $ fold [ "Failed to parse version ", val ]
+        throwError (error (ParseError.parseErrorMessage msg))
+      Right version ->
+        pure (pure (Exact version))
 
 -- | Resolve the exact version to provide for a tool in the environment, based
 -- | on the action.yml file.
-resolve :: Json -> Tool -> ExceptT Error Effect { tool :: Tool, version :: Version }
+resolve :: Json -> Tool -> ExceptT Error Effect (Maybe { tool :: Tool, version :: Version })
 resolve versionsContents tool = do
   let key = Key.fromTool tool
   field <- getVersionField key
   case field of
-    Exact v -> liftEffect do
+    Nothing -> pure Nothing
+
+    Just (Exact v) -> liftEffect do
       Core.info "Found exact version"
-      pure { tool, version: v }
+      pure (pure { tool, version: v })
 
-    Latest -> liftEffect do
+    Just Latest -> liftEffect do
       Core.info $ fold [ "Fetching latest tag for ", Tool.name tool ]
-
-      Core.info "Decoding versions..."
 
       let
         version = lmap printJsonDecodeError $ (_ .: Tool.name tool) =<< decodeJson versionsContents
-
-      Core.info "Parsing versions..."
-
-      let
         parse = lmap parseErrorMessage <<< Version.parseVersion
 
       case parse =<< version of
@@ -79,4 +79,4 @@ resolve versionsContents tool = do
 
         Right v -> do
           Core.info "Successfully fetched version."
-          pure { tool, version: v }
+          pure (pure { tool, version: v })
